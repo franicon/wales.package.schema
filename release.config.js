c519cad158203b7933c6ee40e9934e6858354d6c
Abayomi Aluko <franicons@gmail.com>
2023-08-17 22:57:08 +0100

    feat: package-setup
    * schema package

---
+++ release.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  branches: "main",
+  repositoryUrl: "https://github.com/franicon/wales.package.schema",
+  plugins: [
+    "@semantic-release/npm",
+    "@semantic-release/github",
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+    [
+      "@semantic-release/changelog",
+      {
+        "changelogFile": "docs/CHANGELOG.md"
+      }
+    ],
+    [
+      "@semantic-release/git",
+      {
+        "assets": ["docs/CHANGELOG.md"]
+      }
+    ],
+  ]
+}
